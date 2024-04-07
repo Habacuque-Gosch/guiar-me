5edae31648ba0efbfd0a0fcca372a7cdbf25bbd6
Habacuque-Gosch <habacuke14@gmail.com>
2024-04-07 12:35:30 -0300

    criando funcionalidade de buscar estabelecimentos

---
+++ apps/estabelecimentos/views/buscar.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from apps.estabelecimentos.models import Estabelecimentos
+from django.contrib import messages
+
+
+
+def buscar(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuário não logado")
+        return redirect('login')
+    
+    estabelecimentos = Estabelecimentos.objects.order_by("data_publicada").filter(publicada=True)
+
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            estabelecimentos = estabelecimentos.filter(nome__icontains=nome_a_buscar)
+
+            # user__email=nome_a_buscar)| profiles_friends.filter(user__first_name=nome_a_buscar) | profiles_friends.filter(user__full_name=nome_a_buscar)
+
+    return render(request, 'vagas/index.html', {"estabelecimentos": estabelecimentos, "palavra": nome_a_buscar})
