39ddd2c01525fe75cee1afdc8e47f6b87191c351
habacuque-gosch <habacuke14@gmail.com>
2024-04-19 07:04:51 -0300

    exibindo estabelecimentos no painel admin

--- apps/estabelecimentos/admin.py
+++ apps/estabelecimentos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from apps.estabelecimentos.models import Estabelecimento
 
-# Register your models here.
+class ListandoEstabelecimentos(admin.ModelAdmin):
+    list_display = ("id", "nome", "local", "publicada")
+    list_display_links = ("id", "nome")
+    search_fields = ("nome",)
+    list_filter = ("categoria",)
+    list_editable = ("publicada",)
+    list_per_page = 20
+
+admin.site.register(Estabelecimento, ListandoEstabelecimentos)
