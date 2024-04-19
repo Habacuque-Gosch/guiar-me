e2ff2d1db35d3328bcbc05f2d3051a5816fd6a88
habacuque-gosch <habacuke14@gmail.com>
2024-04-19 07:06:22 -0300

    exibindo nova app no painel de admin

---
+++ apps/resumo/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from apps.resumo.models import Resumo
+
+class ListandoResumo(admin.ModelAdmin):
+    list_display = ("id", "nome", "usuario")
+    list_display_links = ("id", "nome")
+    search_fields = ("nome",)
+    list_filter = ("sexo", "usuario")
+    # list_editable = ("publicada",)
+    list_per_page = 20
+
+admin.site.register(Resumo, ListandoResumo)
