7c098d5d59c7b5baad20362157f6905e5d9f9481
habacuque-gosch <habacuke14@gmail.com>
2024-04-20 18:26:55 -0300

    criando forms de profile

--- apps/resumo/forms.py
+++ apps/resumo/forms.py
@@ -7,12 +7,13 @@ from apps.resumo.models import Resumo
 class ResumoForms(forms.ModelForm):
     class Meta:
         model = Resumo
+        # exclude = ['usuario','sexo','foto']
         exclude = ['usuario']
 
         widgets = {
-            'nome': forms.TextInput(attrs={'class': 'campo-user'}),
-            'idade': forms.TextInput(attrs={'class': 'campo-user'}),
-            'sexo': forms.Select(attrs={'class': 'form-control'}),
+            'nome': forms.TextInput(attrs={'class': 'campo-user', 'placeholder':'Digite seu nome'}),
+            'idade': forms.TextInput(attrs={'class': 'campo-user', 'placeholder':'Digite sua idade'}),
+            'sexo': forms.Select(attrs={'class': 'campo-user', 'placeholder':'Selecione seu sexo'}),
             'foto': forms.FileInput(attrs={'class': 'upload-img'}),
             # 'usuario': forms.Select(attrs={'class': 'form-control'})
         }
