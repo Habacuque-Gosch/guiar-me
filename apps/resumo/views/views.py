81c4375f01cc53b8a888b185a58b875349808756
habacuque-gosch <habacuke14@gmail.com>
2024-04-20 13:51:58 -0300

    criando feature de preencher o perfil em views da app resumo

--- apps/resumo/views/views.py
+++ apps/resumo/views/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render, redirect, get_list_or_404
-# from apps.usuarios.models import Resumo
-# from apps.vagas.models import Vagas
+from apps.resumo.forms import ResumoForms
 from django.contrib.auth.models import User
 from django.contrib import auth, messages
 
 
+def novo_perfil(request):
+    current_user = request.user
+
+    form = ResumoForms
+
+    if request.method == 'POST':
+
+        form = ResumoForms(request.POST, request.FILES)
+
+        if form.is_valid:
+            resumo = form.save(commit=False)
+            resumo.usuario = current_user
+            resumo.save()
+            # messages.success(request, "salvo com sucesso")
+            return redirect('index')
+
+    return render(request, 'usuarios/cadastro/idade_e_nome.html', {'form': form})
