de72ec1ee8829fcebcdb05c85f991203007a71c9
habacuque-gosch <habacuke14@gmail.com>
2024-04-20 13:51:13 -0300

    criando url para preencher o perfil

--- apps/resumo/urls.py
+++ apps/resumo/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from .views import *
-from django.views.generic import TemplateView
 
 
 
 urlpatterns = [
-    # path('', home, name = 'home'),
+    path('novo-perfil', novo_perfil, name = 'novo_perfil'),
 ]
 
 
