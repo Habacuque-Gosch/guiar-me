4b61ee3db3ea43f34dc187d37d424f2e7b5b67af
habacuque-gosch <habacuke14@gmail.com>
2024-04-20 13:47:45 -0300

    criando regra para o usuário preencher o perfil antes de acessar o index

--- apps/estabelecimentos/views/views.py
+++ apps/estabelecimentos/views/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_list_or_404
+from apps.resumo.forms import ResumoForms
+from django.contrib.auth.models import User
+from django.contrib import auth, messages
 from .views import *
+from apps.resumo.models import Resumo
+
+
 
 def index(request):
+
+    user = request.user
+
+    try:
+        resumo = get_list_or_404(Resumo, usuario=user)
+    except:
+        # messages.error(request, "Preencha seu perfil")
+        return redirect('novo_perfil')
+
     return render(request, 'estabelecimentos/index.html')
