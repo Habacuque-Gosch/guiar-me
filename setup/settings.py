a369fde43560df6f6402e320fa2751a17762a2a7
habacuque-gosch <habacuke14@gmail.com>
2024-05-09 13:28:05 -0300

    instalando o debug toolbar

--- setup/settings.py
+++ setup/settings.py
@@ -27,10 +27,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'apps.usuarios.apps.UsuariosConfig',
     'apps.estabelecimentos.apps.EstabelecimentosConfig',
     'apps.resumo.apps.ResumoConfig',
     'apps.eventos.apps.EventosConfig',
+]
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
 ]
 
 MIDDLEWARE = [
@@ -41,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'setup.urls'
