bbaaa041d9ea570e13e6a68ab8c0795e96e7a057
habacuque-gosch <habacuke14@gmail.com>
2024-05-26 11:27:53 -0300

    debug on

--- setup/urls.py
+++ setup/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('', include('apps.eventos.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG == True:
+if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
         path("__debug__/", include("debug_toolbar.urls")),
