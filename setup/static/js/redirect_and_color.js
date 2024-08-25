c785667994e3f37d9e3013396736ab05c41635b8
habacuque-gosch <habacuke14@gmail.com>
2024-08-25 17:45:31 -0300

    ajustando indentação da função de atribuição de cor

--- setup/static/js/redirect_and_color.js
+++ setup/static/js/redirect_and_color.js
@@ -2,8 +2,8 @@
 
 function redirect_and_color(id) {
 
-    var tag_dashboard = document.getElementById(id)
+  var tag_dashboard = document.getElementById(id)
 
-    tag_dashboard.style.backgroundColor = "var(--destaque)"
+  tag_dashboard.style.backgroundColor = "var(--destaque)"
 
-  }
+}
