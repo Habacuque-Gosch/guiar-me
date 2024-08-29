c0f434696392baf951a1f4567b940dea3551ea09
habacuque-gosch <habacuke14@gmail.com>
2024-08-29 14:08:30 -0300

    ajustando script para o front das abas no cardapio mobile

--- setup/static/js/mobile/btn_cardapio_avaliacao_mobile.js
+++ setup/static/js/mobile/btn_cardapio_avaliacao_mobile.js
@@ -5,14 +5,15 @@ function show_reviews() {
     var id_cardapio_btn = document.getElementById("btn-cardapio-mobile")
     var id_reviews = document.getElementById("hide-reviews-mobile")
     var id_reviews_btn = document.getElementById("btn-reviews-mobile")
-    
+
 
     id_cardapio.style.display = 'none'
     id_cardapio_btn.className = 'btn-opcao-desativado-cardapio-mobile'
     id_reviews.style.display = 'flex'
     id_reviews_btn.className = 'btn-opcao-ativado-cardapio-mobile'
-
-
+    
+    id_cardapio_btn.style.borderRadius = '15px 0px 0px 15px'
+    id_reviews_btn.style.borderRadius = '0px 15px 15px 0px'
 }
 
 function show_produtos() {
@@ -26,5 +27,4 @@ function show_produtos() {
     id_reviews.style.display = 'none'
     id_reviews_btn.className = 'btn-opcao-desativado-cardapio-mobile'
     id_cardapio_btn.className = 'btn-opcao-ativado-cardapio-mobile'
-
 }
