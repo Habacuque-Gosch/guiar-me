f042d734d222532f8d927e088b1b6cfe6af57829
habacuque-gosch <habacuke14@gmail.com>
2024-09-07 20:05:31 -0300

    commit in db

--- apps/estabelecimentos/views/buscar.py
+++ apps/estabelecimentos/views/buscar.py
@@ -17,25 +17,21 @@ def buscar(request):
         if nome_a_buscar:
             estabelecimentos = estabelecimentos.filter(nome__icontains=nome_a_buscar)
 
-            # user__email=nome_a_buscar)| profiles_friends.filter(user__first_name=nome_a_buscar) | profiles_friends.filter(user__full_name=nome_a_buscar)
-
     return render(request, 'estabelecimentos/index.html', {"estabelecimentos": estabelecimentos, "palavra": nome_a_buscar})
 
 
-# def filtrar(request, estabelecimento_nome):
+# def filtrar(request):
 
 #     estabelecimentos = Estabelecimento.objects.order_by("data_publicada").reverse().filter(publicada=True)
 
-#     # categorias = Estabelecimento.categoria
+#     # if "tipo" in request.GET:
+#     key_a_buscar = request.GET['tipo']
+#     if key_a_buscar:
+#         estabelecimentos = estabelecimentos.filter(tipo_de_vaga__icontains=key_a_buscar)
 
-#     # print(categorias)
-
-#     if request.GET:
-#         nome_a_buscar = estabelecimento_nome
-#         print(nome_a_buscar)
-#         if nome_a_buscar:
-#             estabelecimentos = estabelecimentos.filter(nome__icontains=nome_a_buscar)
-
-#             # user__email=nome_a_buscar)| profiles_friends.filter(user__first_name=nome_a_buscar) | profiles_friends.filter(user__full_name=nome_a_buscar)
+#     # if "local" in request.GET:
+#     key_a_buscar = request.GET['local']
+#     if key_a_buscar:
+#         estabelecimentos = estabelecimentos.filter(categoria__icontains=key_a_buscar)
 
 #     return render(request, 'estabelecimentos/index.html', {"estabelecimentos": estabelecimentos})
